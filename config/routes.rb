afb8ea367412a22e242d14995a897136a2ba46d7
CorentinChen <corentin.chentouf@gmail.com>
2019-11-14 14:57:11 +0100

    exV2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  resources :cocktails, only: [:index, :show, :create, :new] do
+    resources :doses, only: [:create, :new, :destroy]
+  end
 end
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
